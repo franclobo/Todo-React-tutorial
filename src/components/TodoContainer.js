0f1c91f62a3b683d5f98ca705b7f94418b9f6e37
franclobo <fjbl2788@gmail.com>
2022-06-13 12:47:45 -0500

    Update index.js

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,10 +4,10 @@ class TodoContainer extends React.Component {
   render() {
     return (
       <div>
-      <h1>Hello from Create React App</h1>
-      <p>I am in a React Component!</p>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
       </div>
-    )
+    );
   }
 }
 
