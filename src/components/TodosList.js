52b096dd33cc63253115a2dcfbd2161ae0e06b52
franclobo <fjbl2788@gmail.com>
2022-06-13 16:19:52 -0500

    Change class components TodoItem and TodoList to function components.

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    );
-  }
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
