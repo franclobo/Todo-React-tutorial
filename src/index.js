9f3fe87e8b2fca403a78d0951a1b8d379b27850f
franclobo <fjbl2788@gmail.com>
2022-06-14 12:52:29 -0500

    Add basename={process.env.PUBLIC_URL} in index.js.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
